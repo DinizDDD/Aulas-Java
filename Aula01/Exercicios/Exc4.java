45a7f0373b487c164bd555d57e77be46034bcc52
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-03-18 09:35:39 -0300

    update the exc04

---
+++ Aula01/Exercicios/Exc4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exemplo001{
+    public static void main(String args[]){
+        Scanner leia = new Scanner (System.in);
+
+        int age = "";
+        System.out.print("Qual a idade?");
+        leia .nextInt();
+
+        if (age >= 18){
+            System
+        }
+
+
+
+  
+       
+
+        leia.close();
+    }
+}
