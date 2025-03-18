c126dae68f2c34f23c8498e1ccbad76bfa24af39
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-03-18 09:01:29 -0300

    Exc03 make

---
+++ Aula01/Exercicios/Exc03.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Exemplo001{
+    public static void main(String args[]){
+        Scanner leia = new Scanner (System.in);
+
+        int n1 = 0;
+        System.out.println("Qual a primeira nota desse aluno?");
+        n1 = leia.nextInt();
+
+        int n2 = 0;
+        System.out.println("Qual a segunda nota desse aluno?");
+        n2 = leia.nextInt();
+
+        int n3 = 0;
+        System.out.println("Qual a terceira nota desse aluno?");
+        n3 = leia.nextInt();
+
+        float media = 0;
+        media = (n1+n2+n3) / 3;
+
+        
+        if (media >= 7){
+            System.out.println("Aluno aprovado!");
+        }
+
+        else {
+            System.out.println("Aluno reprovado!");
+        }
+
+       
+
+        leia.close();
+    }
+}
