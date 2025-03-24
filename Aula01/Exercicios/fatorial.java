bb84fb54190cd4746ae773debb53433cab074811
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-03-24 08:01:22 -0300

    bro I am cooked

--- Aula01/Exercicios/fatorial.java
+++ Aula01/Exercicios/fatorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalculaFatorial {
+public class fatorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
--- Aula01/Exercicios/salario.java
+++ Aula01/Exercicios/salario.java
@@ -10,24 +10,24 @@ public class salario{
 
         double pagamento = 0;
 
-        if (salario <= 2.259,20){
+        if (salario <= 2259.20){
             System.out.println("Seu salario não recebe imposto")
         } else{
             if (salario <= 2.826,65){
                 System.out.println("Seu salario recebe 	7,5% de impostos")
-                pagamento = salario * 0,075;
+                pagamento = salario * 0.075;
             } else {
                 if (salario <= 3.751,05){
                     System.out.println("Seu salario recebe 15% de impostos")
-                    pagamento = salario * 0,15;
+                    pagamento = salario * 0.15;
                 } else {
                     if (salario <= 4.664,68){
                         System.out.println("Seu salario recebe 22,5% de impostos")  
-                        pagamento = salario * 0,225;
+                        pagamento = salario * 0.225;
                     } else {
                         if (salario > 4.664,68){
                             System.out.println("Seu salario recebe 27,5% de impostos")
-                            pagamento = salario * 0,275;
+                            pagamento = salario * 0.75;
                         }                      
                     }
                 }
