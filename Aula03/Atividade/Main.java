a5fcc7e11331b40d1361ec59e013f0e6472b6a9a
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-04-14 07:54:25 -0300

    basically finished

--- Aula03/Atividade/Main.java
+++ Aula03/Atividade/Main.java
@@ -8,9 +8,32 @@ public class Main {
 
         Product produto1 = new Product("Almondega", 10.00);
 
-        System.out.println(produto1.getName()); 
+        // o atributo nome, é public, então ele funciona das duas formas
+        System.out.println(produto1.name);
+        System.out.println(produto1.getNome());
+
+        // agora o preço, é private, então ele só printa utilizando o getPrice()
+        // System.out.println(produto1.price);
+        System.out.println(produto1.getPrice());
 
 
+
+        // Assim como no exemplo anterior, o nome é alterado das duas formas por ser public
+        produto1.setNome("Davi");
+        produto1.name = "Eduardo";
+
+        // agora o preço que é private, só funciona com o set
+        produto1.setPrice(25);
+        // produto1.price = 30;
+
+        
+        
+        // Porém, se eu fizer um metódo para printar o preço, ele irá aparecer do mesmo jeito
+        produto1.Info();
+
+        // Isso também funciona para alterar o valor do atributo
+        produto1.Mudar();
+        System.out.println(produto1.getPrice());
 
 
         leia.close();
--- Aula03/Atividade/Product.java
+++ Aula03/Atividade/Product.java
@@ -1,25 +1,44 @@
 package Aula03.Atividade;
 
-public class Product {
-    // Primeiro passo criei os atributos dessa classe
-    String name;
-    double price;
+class Product {
+    public String name;
+    private double price;
 
-    // Depois, criei o construtor para a classe
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    // Apenas uma graça para ver o valor dos atributos
     void Info() {
         System.out.println("Nome: " + name);
         System.out.println("Preço: " + price);
     }
 
-    public String getName() {
+    void Mudar() {
+        this.price = 9999.0;
+
+    }
+
+    public String getNome() {
         return name;
     }
+
+    public void setNome(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+        } else {
+            System.out.println("Preço inválido");
+        }
+    }
+
 
     
 }
