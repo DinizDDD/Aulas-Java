5eae9467c367c07b719784f24df13d60ef24e201
DadiAugusto <davidinizdalevedo9@gmail.com>
2025-08-05 13:44:35 -0300

    create impapar

---
+++ 2BIM/Excs1/impapar.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+    public class tripledouble{
+        public static void main(String args[]){
+            Scanner leia = new Scanner (System.in);
+
+            int n1 = 0;
+            System.out.println("Informe o número:");
+            leia.nextInt();
+
+            if (n1 % 2 == 0){
+                System.out.println("Esse número é par");
+            } else {
+                System.out.println("Esse número é impar");
+            }
+    
+
+
+
+
+            leia.close();
+        }
+    }
+
